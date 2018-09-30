f1724c6e88fe5bd673b5269a1d1f5ebac078a8ea
DESKTOP-66V6JV5\PC01 <sijm@naver.com>
2018-09-30 21:48:55 +0900

    test

---
+++ src/main/java/com/example/demo/Nayha20180930Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nayha20180930Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nayha20180930Application.class, args);
+	}
+}
