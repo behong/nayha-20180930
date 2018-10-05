f3be62ded703ea0223bf6550c5c04f0f89485e9d
juf <juf@DESKTOP-66V6JV5>
2018-10-05 20:50:06 +0900

    db 없이 mvc Test

--- src/main/java/com/example/web/WelcomeController.java
+++ src/main/java/com/example/web/WelcomeController.java
@@ -1,15 +1,17 @@
 package com.example.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WelcomeController {
 	
-	@GetMapping("/helloworld")
-	public String welcome(String name) {
+	@GetMapping("/test")
+	public String welcome(String name, Model model) {
 		System.out.println("11111 name : " + name);
 		//Model.add
+		model.addAttribute("name" , name);
 		return "welcome";
 	}
 
