3a2b95c239ac047720e530e4ed55c3f73f7edf9e
DESKTOP-66V6JV5\PC01 <sijm@naver.com>
2018-10-07 01:49:55 +0900

    회원정보 등록 / 수정 구현 완료

---
+++ src/main/java/com/example/domain/User.java
@@ -0,0 +1,83 @@
+package com.example.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+//Entity 데이터 베이스와 연동 맵핑 어노테이션
+@Entity
+public class User {
+
+	
+	@Id
+	@GeneratedValue  //자동으로 1씩 증가
+	private Long id;
+	
+	@Column(nullable=false,length=20)
+	private String userid;
+	
+	private String password;
+	private String name;
+	private String email;
+	
+	
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void update(User newUser) {
+		this.password = newUser.password;
+		this.name = newUser.name;
+		this.email = newUser.email;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", userid=" + userid + ", password=" + password + ", name=" + name + ", email="
+				+ email + "]";
+	}
+	
+
+
+
+
+	
+	
+	
+
+}
--- src/main/java/com/example/web/UserController.java
+++ src/main/java/com/example/web/UserController.java
@@ -1,29 +1,67 @@
 package com.example.web;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.User;
+import com.example.domain.UserRepository;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
 	
-	private List<User> users = new ArrayList<User>();
+	//private List<User> users = new ArrayList<User>();
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@GetMapping("/form")
+	public String form() {
+		return "user/form";
+	}
 	
 	@PostMapping("/create")
 	public String create(User user) {
-		System.out.println("user " + user );
-		users.add(user);
-		return "redirect:/list";
+		//System.out.println("user " + user );
+		//users.add(user);
+		userRepository.save(user);
+		return "redirect:/user/list";
 	}
 	
 	@GetMapping("/list")
 	public String list(Model model) {
-		model.addAttribute("users",users);
-		return "list";
+		//model.addAttribute("users",users);
+		model.addAttribute("users",userRepository.findAll());
+		return "/user/list";
+	}
+	
+	@GetMapping("/{updateId}/form")
+	public String updateForm(@PathVariable Long updateId,Model model) {
+		//findone 애러 JpaRepository findone 미구현..??
+		//User user = userRepository.getOne(updateId);
+		User user = userRepository.findById(updateId).get();
+		//System.out.println("rtn_user " + user );
+		model.addAttribute("rtn_user", user);
+		return "/user/updateForm";
+	}
+	
+	//@PostMapping("/update")
+	@PutMapping("/update")
+	public String userUpdate(User newUser) {
+		System.out.println("newUser" + newUser );
+		//전달된 ID로  현재 db 읽어오기
+		User user = userRepository.findById(newUser.getId()).get();
+		//변경
+		user.update(newUser);
+		//변경 DB 반영
+		userRepository.save(user);
+		return "redirect:/user/list";
 	}
 
 }
