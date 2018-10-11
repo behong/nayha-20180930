3e7a112ef03699a3737a86f3158b60fa2ac92558
DESKTOP-66V6JV5\PC01 <sijm@naver.com>
2018-10-11 23:51:07 +0900

    질문 상세 기능 구현

--- src/main/java/com/example/domain/Question.java
+++ src/main/java/com/example/domain/Question.java
@@ -1,30 +1,43 @@
 package com.example.domain;
 
-import javax.persistence.Column;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 //Entity 데이터 베이스와 연동 맵핑 어노테이션
 @Entity
 public class Question {
-
 	
 	@Id
 	@GeneratedValue  //자동으로 1씩 증가
 	private Long id;
+	//회원은 여러개 질문
+	//질문은 회원 한개 매핑
 	
-	private String writer;
+	@ManyToOne
+	@JoinColumn(foreignKey = @ForeignKey(name="fk_question_writer"))
+	private User writer;
+	
 	private String title;
 	private String contents;
+	
+	private LocalDateTime createDate;
 
 	public Question() {}
 	
-	public Question(String writer, String title, String contents) {
+	public Question(User writer, String title, String contents) {
 		super();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 		this.writer = writer;
 		this.title = title;
 		this.contents = contents;
+		this.createDate = createDate.now();
 	}
 
 
--- src/main/java/com/example/web/QuestionController.java
+++ src/main/java/com/example/web/QuestionController.java
@@ -4,7 +4,9 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -38,12 +40,19 @@ public class QuestionController {
 		}
 		
 		User sessionuser = HttpSessionUtils.getUserFromSession(session);
-		Question newQuestion = new Question(sessionuser.getUserid(), title, contents);
+		Question newQuestion = new Question(sessionuser, title, contents);
 		
 		QuestionRepository.save(newQuestion);
 		
 		return "redirect:/";
 	}
 	
+	@GetMapping("/show/{id}")
+	public String show(@PathVariable Long id, Model model) {
+		
+		model.addAttribute("questions",QuestionRepository.findById(id).get() );
+		
+		return "/qna/show";
+	}
 
 }
